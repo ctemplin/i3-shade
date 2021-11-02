426094e2d67b70d7f0272fccb3f1f547162e418b
Templin, Chris <ctemplin@gmail.com>
2021-11-02 12:03:48 -0400

    Cleaning up and commenting main float-con-handling loop. Extracting mark test to function. Removing pointless conditional.

--- src/index.js
+++ src/index.js
@@ -40,31 +40,36 @@ const handleWindowEvent = async function() {
     // Tiled window focused
     if (['user_off', 'auto_off'].includes(float_val) ) {
       i3.tree((err, resp) => {
+        //Get all the floating containers on the current workspace
         let fnodes = resp.
         nodes.find(_ => _.type == 'output' && _.name != '__i3').
         nodes.find(_ => _.type == 'con' && _.name == 'content').
         nodes.find(_ => _.type == 'workspace' && _.num == fcsdWsNum).
         floating_nodes.flatMap(_ => _.nodes);
-        if (fnodes.length) {
-          fnodes.filter(node => !node.marks.some( mark => mark.startsWith(exemptMarkPref))).map(node => {
-            if (true) {
-              let winHeight = node.rect.height + node.deco_rect.height - peekMargin
-              i3.command(
-                util.format(
-                  '[con_id=%s] mark --add %s%d_%d_%s, move position %d px -%d px', 
-                  node.id, markPref, node.rect.x, node.rect.y, node.id, node.rect.x, winHeight
-                ),
-                function(err, resp) { console.log(err, resp) }  
-              )
-            }
-          })
+        // Test of window eligiblility (neither exempted nor already shaded)
+        const isUnmarked = function(node) {
+          return !node.marks.some( _ => 
+            _.startsWith(markPref) || _.startsWith(exemptMarkPref)
+          )
         }
+        // Filter for eligble windows and loop through them.
+        fnodes.filter(node => isUnmarked(node)).map(node => {
+          console.log('loop', fcsdWsNum, Date())
+          let winHeight = node.rect.height + node.deco_rect.height - peekMargin
+          let markCom = util.format(
+            '[con_id=%s] mark --add %s%d_%d_%s, move position %d px -%d px',
+            node.id, markPref, node.rect.x, node.rect.y, node.id, node.rect.x, winHeight
+          )
+          i3.command(markCom, (err, resp) => {
+            // console.log(err, resp)
+          })
+        })
       })
     }
     // Floating window focused
     else {
       let foccon = arguments[0].container
-      let mark = foccon.marks.find(mark => mark.startsWith(markPref) && !mark.startsWith(exemptMarkPref))
+      let mark = foccon.marks.filter(mark => mark.startsWith(markPref) && !mark.startsWith(exemptMarkPref))[0]
       if (mark) {
         let toks = mark.split("_")
         i3.command(
