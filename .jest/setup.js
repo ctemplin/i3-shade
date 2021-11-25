0e04f8dbbf9e02eb2df78621ce602d549d38c047
Templin, Chris <ctemplin@gmail.com>
2021-11-25 16:07:55 -0500

    Mock i3 server refactor Integrating underused event handlers into handleMessage. Renaming outgoing 'payload' var to 'resp' for clarity.

--- .jest/setup.js
+++ .jest/setup.js
@@ -10,24 +10,6 @@ beforeAll(() => {
   globals.__SOCKET_PATH__ = spath
 
   server = new I3MockServer(handleMessage)
-  // code 0
-  server.i3ipc.on('workspace', (wsNum) => {
-    var payload = require('../tests/data-mocks/ev_workspace_focus.json')
-    payload.current.num = wsNum
-    server._stream.write(encodeCommand(eventCodeFromName['workspace'], JSON.stringify(payload)))
-  })
-
-  // code 5
-  server.i3ipc.on('binding', () => {
-    var payload = require('../tests/data-mocks/ev_binding_shade-exempt.json')
-    server._stream.write(encodeCommand(eventCodeFromName['binding'], JSON.stringify(payload)))
-  })
-
-  // code 3
-  server.i3ipc.on('window', () => {
-    var payload = require('../tests/data-mocks/ev_window_focus.json')
-    server._stream.write(encodeCommand(eventCodeFromName['window'], JSON.stringify(payload)))
-  })
 })
 
 afterAll(() => {
@@ -36,27 +18,32 @@ afterAll(() => {
 
 function handleMessage(message) {
   let comCode = message.code
-  payload = message.payload?.toString()
+  let payload = message.payload?.toString()
+  let resp
   switch(commandNameFromCode[comCode]) {
     case 'GET_WORKSPACES':
-      payload = require('../tests/data-mocks/cm_workspaces_initial.json')
-      server._stream.write(encodeCommand(comCode, JSON.stringify(payload)))
+      resp = require('../tests/data-mocks/cm_workspaces_initial.json')
+      server._stream.write(encodeCommand(comCode, JSON.stringify(resp)))
       break;
     case 'GET_TREE':
-      payload = require('../tests/data-mocks/cm_tree.json')
-      server._stream.write(encodeCommand(comCode, JSON.stringify(payload)))
+      resp = require('../tests/data-mocks/cm_tree.json')
+      server._stream.write(encodeCommand(comCode, JSON.stringify(resp)))
       break;
     case 'COMMAND':
       if (payload == globals.__EXEMPT_COM__) {
-        server.i3ipc.emit('binding')
+        resp = require('../tests/data-mocks/ev_binding_shade-exempt.json')
+        server._stream.write(encodeCommand(eventCodeFromName['binding'], JSON.stringify(resp)))
       }
       let payloadSegs = payload.split(' ')
       if (payloadSegs[0] == 'workspace') {
         var wsNum = Number(payloadSegs.pop())
-        server.i3ipc.emit('workspace', wsNum)
+        resp = require('../tests/data-mocks/ev_workspace_focus.json')
+        resp.current.num = wsNum
+        server._stream.write(encodeCommand(eventCodeFromName['workspace'], JSON.stringify(resp)))
       }
       if (payload == "focus mode_toggle") {
-        server.i3ipc.emit('window', payload)
+        resp = require('../tests/data-mocks/ev_window_focus.json')
+        server._stream.write(encodeCommand(eventCodeFromName['window'], JSON.stringify(resp)))
       }
       if (payloadSegs[1] == "mark") {
 
